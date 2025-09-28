d75a29529c876b700fd7c5ef3d13d64eef2888c0
YuzheLin0310 <yuzhelin@my.yorku.ca>
2025-09-28 16:22:37 -0400

    Update BookstoreAPI.java

--- src/main/java/com/university/bookstore/api/BookstoreAPI.java
+++ src/main/java/com/university/bookstore/api/BookstoreAPI.java
@@ -3,26 +3,22 @@
 // (powered by FernFlower decompiler)
 //
 
-package src.main.java.com.university.bookstore.api;
-
 public interface BookstoreAPI {
     static {
         throw new Error("Unresolved compilation problems: \n\tThe type java.lang.Object cannot be resolved. It is indirectly referenced from required .class files\n\tThe declared package \"com.university.bookstore.api\" does not match the expected package \"src.main.java.com.university.bookstore.api\"\n\tThe import java.util cannot be resolved\n\tThe import com cannot be resolved\n\tBook cannot be resolved to a type\n\tString cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tString cannot be resolved to a type\n\tList cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tString cannot be resolved to a type\n\tList cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tString cannot be resolved to a type\n\tList cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tList cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tBook cannot be resolved to a type\n\tList cannot be resolved to a type\n\tBook cannot be resolved to a type\n");
     }
 
-    boolean add(Book var1);
+    boolean add(Book book);
 
-    boolean removeByIsbn(String var1);
+    boolean removeByIsbn(String isbn);
 
-    Book findByIsbn(String var1);
+    Book findByIsbn(String isbn);
 
-    List<Book> findByTitle(String var1);
+    List<book> findByTitle(String title);
 
-    List<Book> findByAuthor(String var1);
+    List<book> findByAuthor(String author);
 
-    List<Book> findByPriceRange(double var1, double var3);
-
-    List<Book> findByYear(int var1);
+    List<book> findByPriceRange(double priceMin, double priceMax);
 
     int size();
 
